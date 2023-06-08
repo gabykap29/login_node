4dfb94f6e736d7cdc9ba9e7607bf53e1562b8eaf
Gabriel Acosta <gabriacos@gmail.coma>
2023-06-08 16:32:42 -0300

    Primer commit del login

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+//motor de platillas
+app.set('view engine', 'ejs');
+
+//seteo de carpeta public para archivos estaticos
+
+app.use(express.static('public'));
+
+//procesamiento de datos
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//variables de entorno
+
+dotenv.config({path:'./env/.env'})
+
+//trabajar con las cookies
+
+// app.use(cookieParser);
+
+//llamar al router
+app.use('/', require('./routes/router'))
+
+
+//poner el marcha el server
+app.listen(3000, ()=>{
+    console.log('server up http://localhost:3000') 
+})
